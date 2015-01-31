a1591ede172fe7f4f503d9a1975d8cf3f11d22b8
lazandrei19 <lazandrei19@yahoo.com>
2015-01-31 10:59:46 +0200

    INITIAL RELEASE

--- src/com/lazandrei19/game/util/Background.java
+++ src/com/lazandrei19/game/util/Background.java
@@ -3,10 +3,26 @@ package com.lazandrei19.game.util;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.Color;
 import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+import java.io.IOException;
 
 public class Background implements Drawable {
     int x, y, w = Display.getWidth(), h = Display.getHeight();
     Texture t;
+
+    public Background() {
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void init() throws IOException {
+        t = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/tex/pht1024.png"));
+    }
 
     @Override
     public double getX() {
